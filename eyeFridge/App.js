a89224cc1567752d0a4bdf946574150d1d449832
AdamRzem <adamrzem09@gmail.com>
2024-04-18 14:07:09 +0200

    slight changes

--- eyeFridge/App.js
+++ eyeFridge/App.js
@@ -5,24 +5,8 @@ import Nav from './components/Navigation'
 import Main from './components/Card';
 import Theme from './components/Theme';
 import { useColorScheme } from 'nativewind';
-// import { NavigationContainer } from '@react-navigation/native';
 import PushNotification from 'react-native-push-notification';
 
-// const scheduleNotification = (title, message, delay) => {
-//   PushNotification.localNotification({
-//     // Mandatory fields
-//     title, // Required
-//     message, // Required
-
-//   });
-// };
-
-
-// const scheduleExpiryNotification = (title, expiryDate) => {
-//   const today = new Date();
-//   const notificationDelay = Math.floor((expiryDate - today) / (1000 * 60 * 60 * 24)); // Calculate delay in days
-//   scheduleNotification(title, 'This item is expiring soon!', notificationDelay);
-// };
 
 const lightTheme = {
   backgroundColor: '#f4f3f4', // Light background
@@ -37,7 +21,7 @@ const darkTheme = {
 };
 
  const App = () => {
-  const {colorScheme, toggleColorScheme} = useColorScheme();
+
   const [currentTheme, setCurrentTheme] = useState(lightTheme); 
 
   const toggleSwitch = () => {
@@ -45,18 +29,14 @@ const darkTheme = {
     setCurrentTheme(isEnabled ? lightTheme : darkTheme); 
   };
   
-//   useEffect(() => {
-//     registerForPushNotificationsAsync();
-// }, []);
 
   const [isEnabled, setIsEnabled] = useState(false);
-  // const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
+  
 
   const _handleMore = () => console.log('Shown more');
 
   return (
-    <View style={{flex: 1, flexDirection: 'column', backgroundColor: currentTheme.backgroundColor, textColor: currentTheme.textColor}} >
+    <View style={{flex: 1, flexDirection: 'column', backgroundColor: currentTheme.backgroundColor, textColor: currentTheme.textColor, paddingTop: 20, paddingBottom: 20}} >
       <Appbar style={{marginBottom: 10, backgroundColor: currentTheme.backgroundColor, textColor: currentTheme.textColor}} >
       <Appbar.BackAction onPress={() => (console.log('dziala'))} />
       <Appbar.Content title="eyeFridge" />
@@ -73,15 +53,6 @@ const darkTheme = {
       <Main title={'Brakujące produkty: '} />
       <Main title={'Przeterminowane rzeczy: '} />
       
-
-      {/* <Title>{title}</Title> */}
-      {/* Display expiry information for items */}
-      {/* {items.map((item) => (
-        <View key={item.id}>
-          <Text>{item.name} - Expires: {item.expiryDate}</Text>
-          {scheduleExpiryNotification(item.name, item.expiryDate)}
-        </View>
-      ))} */}
  
       {/* <Nav /> */}
       </ScrollView>
